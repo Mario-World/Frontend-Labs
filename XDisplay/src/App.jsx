6b273929515c1c9757723f80e2018d69c647e32b
Mario-World <pavansaigudelli01@gmail.com>
2025-10-13 19:59:54 +0530

    first commit

---
+++ XDisplay/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import NameForm from "./NameForm";
+
+export default function App() {
+  return (
+    <div>
+      <NameForm />
+    </div>
+  );
+}
---
+++ XDisplay/src/NameForm.jsx
@@ -0,0 +1,58 @@
+import React, { useState }from 'react'
+
+export default function NameForm() {
+
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [fullName, setFullName] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault(); //prevent page reload
+       if(firstName && lastName) {
+        setFullName(`${firstName} ${lastName}`);
+       } else {
+        setFullName("please enter both first and last name.");
+       }
+    };
+
+  return (
+    <div style={{ fontFamily: "Arial", padding: "20px"}}>
+    <h2>Enter your Name</h2>
+
+    <form onSubmit={handleSubmit}>
+     <label htmlFor="firstName"> First Name: </label>
+     <input
+     id="firstName"
+     type="text"
+     value={firstName}
+     onChange={(e) => setFirstName(e.target.value)}
+     required
+
+      /> 
+     <br /><br /> 
+
+     <label htmlFor="lastName"> Last Name: </label>
+     <input 
+      id="lastName"
+      type="text"
+      value={lastName}
+      onChange={(e) => setLastName(e.target.value)}
+      required
+     
+    />
+
+    <br /><br />
+
+    <button type="submit">Submit</button>
+    
+    </form>
+
+    {fullName && (
+        <h3 style={{ marginTop: "20px"}}>
+        Full Name: {fullName}
+        </h3>
+    )}
+    </div>
+  );
+}
+
