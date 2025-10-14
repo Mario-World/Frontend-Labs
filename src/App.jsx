ba556d5e2bef3be70c8d8df64cfc8c76f36a4791
Mario-World <pavansaigudelli01@gmail.com>
2025-10-14 15:36:22 +0530

    counter app initiated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import NameForm from './NameForm';
+import  { useState } from 'react';
+
+
 
 export default function App() {
+
+  const [count, setCount] = useState(0);
+
+  const handleIncrement = () => setCount(count + 1);
+  const handleDecrement = () => setCount(count - 1);
+
   return (
     <div>
-    <NameForm />
+      <h2>Counter App</h2>
+      <p>Count: {count}</p>
+      <button onClick={handleIncrement}>Increment</button>
+      <button onClick={handleDecrement}>Decrement</button>
+
+
     </div>
-  )
+  );
 }
